f01cda0b6c226e1d2e37232182f2f9a455f9d6ed
basha11505 <shaikbasha11512@gmail.com>
2025-12-16 22:33:05 +0530

    message

---
+++ day1.js
@@ -0,0 +1,52 @@
+//basics day 1
+let a =10;
+let b =25;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+//second programme
+let x = 5;
+
+let add = x + 3;
+console.log(add); // 8
+
+let sub = x - 2;
+console.log(sub); // 3
+
+let mul = x * 2;
+console.log(mul); // 10
+
+let div = x / 3;
+console.log(div); // 1.6666666666666667
+
+//3rd programme
+let n = 5;
+//n++; // only either increment or decrement will work 
+//n--; // only either increment or decrement will work 
+console.log(n);
+console.log(typeof n);
+ //4th programme
+let marks =45;
+    if(marks>=45){
+        console.log("fail");
+    }else{
+        console.log("pass");
+    }
+    //5th programme
+let day = 4;
+
+switch (day) {
+case 1:
+    console.log("Monday");
+    break;
+case 2:
+    console.log("Tuesday");
+    break;
+case 3:
+    console.log("Wednesday");
+    break;
+default:
+    console.log("Invalid day");
+}  
